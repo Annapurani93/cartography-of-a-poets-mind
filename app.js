47b7be88ccb9818a75fb026cfb9a1c4e54923a13
Annapurani93 <51084584+Annapurani93@users.noreply.github.com>
2026-06-20 13:10:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-alert("Hello from app.js");
+const API =
+  "https://public-api.wordpress.com/rest/v1.1/sites/annapurani93.wordpress.com/posts";
+
+async function loadPoems() {
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+
+    const poemsDiv = document.getElementById("poems");
+
+    poemsDiv.innerHTML = "";
+
+    data.posts.forEach(post => {
+      const card = document.createElement("div");
+
+      card.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.date.substring(0,10)}</p>
+        <hr>
+      `;
+
+      poemsDiv.appendChild(card);
+    });
+  }
+
+  catch (error) {
+    document.getElementById("poems").innerHTML =
+      "Could not load poems.";
+    console.error(error);
+  }
+}
+
+loadPoems();
